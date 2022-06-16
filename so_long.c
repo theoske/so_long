7e782ee85d96e6765829ce89bcfcefd0d28efd0c
Theo Kempf elbaz <tkempf-e@k0r2p4.42mulhouse.fr>
2022-06-16 17:14:52 +0200

    maj

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: tkempf-e <tkempf-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 14:53:32 by tkempf-e          #+#    #+#             */
-/*   Updated: 2022/06/15 16:44:27 by tkempf-e         ###   ########.fr       */
+/*   Updated: 2022/06/16 17:14:33 by tkempf-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,47 @@
 #include <stdlib.h>
 #include <math.h>
 
-int *move(void *mlx, void *mlx_win)
+typedef struct s_vars
 {
-    static int  i = 0;
-    
-    mlx_pixel_put(mlx, mlx_win, i, i, 15033088);
-    i++;
-    return (0);
+	void	*mlx;
+	void	*mlx_win;
+}	t_vars;
+
+
+int key_hook(int keycode, t_vars *vars)
+{
+	static int	x = 700;
+	static int	y = 700;
+
+	if (keycode == 13)
+		y++;
+	else if (keycode == 1)
+		y--;
+	else if (keycode == 2)
+		x++;
+	else if (keycode == 0)
+		x--;
+	printf("%d\n", keycode);
+	mlx_pixel_put(vars->mlx, vars->mlx_win, x, y, 0xc52907);
+	return (0);
 }
 
-int main()
+int main()//utiliser images
 {
-    void    *mlx;
-    void    *mlx_win;
-    int     i;
-    int     j;
-    int     *ptrmove;
+	t_vars	vars;
+	int		i;
+	int		j;
     
-    mlx = mlx_init();
-    mlx_win = mlx_new_window(mlx, 1440, 1080, "win");
-    i = 0;
-    while (i++ < 500)
-    {
-        j = 0;
-        while (j++ < 500)
-            mlx_pixel_put(mlx, mlx_win, i, j, 7430598);
+	vars.mlx = mlx_init();
+	vars.mlx_win = mlx_new_window(vars.mlx, 1440, 1080, "win");
+	i = 0;
+	while (i++ < 500)
+	{
+		j = 0;
+		while (j++ < 500)
+			mlx_pixel_put(vars.mlx, vars.mlx_win, i, j, 7430598);
     }
-    mlx_key_hook(mlx, move(mlx, mlx_win), 0);
-    mlx_loop(mlx);
-    return (0);
+	mlx_key_hook(vars.mlx_win, key_hook, &vars);
+	mlx_loop(vars.mlx);
+	return (0);
 }
