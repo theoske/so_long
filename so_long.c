e8ebd0add15d94a7613ec51da95e99674f58c93d
Theo Kempf elbaz <tkempf-e@k0r3p5.42mulhouse.fr>
2022-06-20 15:06:52 +0200

    maj

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: tkempf-e <tkempf-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 14:53:32 by tkempf-e          #+#    #+#             */
-/*   Updated: 2022/06/16 17:14:33 by tkempf-e         ###   ########.fr       */
+/*   Updated: 2022/06/20 15:06:31 by tkempf-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,43 +19,56 @@ typedef struct s_vars
 {
 	void	*mlx;
 	void	*mlx_win;
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
 }	t_vars;
-
 
 int key_hook(int keycode, t_vars *vars)
 {
-	static int	x = 700;
-	static int	y = 700;
+	static int	x = 100;
+	static int	y = 100;
 
 	if (keycode == 13)
-		y++;
+		y -= 100;
 	else if (keycode == 1)
-		y--;
+		y += 100;
 	else if (keycode == 2)
-		x++;
+		x += 100;
 	else if (keycode == 0)
-		x--;
-	printf("%d\n", keycode);
-	mlx_pixel_put(vars->mlx, vars->mlx_win, x, y, 0xc52907);
+		x -= 100;
+	mlx_put_image_to_window(vars->mlx, vars->mlx_win, vars->img, x, y);
 	return (0);
 }
 
 int main()//utiliser images
 {
 	t_vars	vars;
+	char	*dst;
 	int		i;
 	int		j;
-    
+	int		x;
+	int		y;
+	
 	vars.mlx = mlx_init();
-	vars.mlx_win = mlx_new_window(vars.mlx, 1440, 1080, "win");
-	i = 0;
-	while (i++ < 500)
+	vars.mlx_win = mlx_new_window(vars.mlx, 900, 900, "so_long");
+	vars.img = mlx_new_image(vars.mlx, 100, 100);
+	vars.addr = mlx_get_data_addr(vars.img, &vars.bits_per_pixel, &vars.line_length, &vars.endian);
+	x = 0;
+	while (x <= 100)
 	{
-		j = 0;
-		while (j++ < 500)
-			mlx_pixel_put(vars.mlx, vars.mlx_win, i, j, 7430598);
-    }
-	mlx_key_hook(vars.mlx_win, key_hook, &vars);
+		y = 0;
+		while (y <= 100)
+		{
+			dst = vars.addr + (y * vars.line_length + x * (vars.bits_per_pixel / 8));
+			*(unsigned int*)dst = 10760863;
+			y++;
+		}
+		x++;
+	}
+	mlx_hook(vars.mlx_win, 2, 1L<<0, key_hook, &vars);
 	mlx_loop(vars.mlx);
 	return (0);
 }
