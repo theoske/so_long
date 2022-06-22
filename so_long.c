3753eedd1854e991f5741aa389a1d70e179c0cb8
Theo Kempf elbaz <tkempf-e@k0r3p6.42mulhouse.fr>
2022-06-22 16:20:19 +0200

    maj

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: tkempf-e <tkempf-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 14:53:32 by tkempf-e          #+#    #+#             */
-/*   Updated: 2022/06/22 15:09:59 by tkempf-e         ###   ########.fr       */
+/*   Updated: 2022/06/22 16:20:13 by tkempf-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,79 @@ typedef struct s_vars
 	int		line_length;
 	int		endian;
 }	t_vars;
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*join;
+	int		i;
+	int		j;
+
+	j = 0;
+	i = 0;
+	join = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!join)
+		return (NULL);
+	while (s1 && s1[i])
+	{
+		join[i] = *(char *)(s1 + i);
+		i++;
+	}
+	while (s2[j])
+	{
+		join[i + j] = *(char *)(s2 + j);
+		j++;
+	}
+	join[i + j] = '\0';
+	free(s1);
+	return (join);
+}
+
+char	*ft_line(char *line, char *buffer, int octet, int fd)
+{
+	while (buffer[0] != '\n' && octet > 0)
+	{
+		octet = read(fd, buffer, 1);
+		buffer[octet] = '\0';
+		line = ft_strjoin(line, buffer);
+	}
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	char		buffer[2];
+	char		*line;
+	int			octet;
+
+	line = NULL;
+	octet = 0;
+	if (octet == 0)
+	{
+		octet = read(fd, buffer, 1);
+		buffer[octet] = '\0';
+		line = ft_strjoin(line, buffer);
+	}
+	if (octet > 0 && buffer[0] != '\0')
+		line = ft_line(line, buffer, octet, fd);
+	else
+	{
+		free(line);
+		return (NULL);
+	}
+	return (line);
+}
 
 int key_hook(int keycode, t_vars *vars)
 {
@@ -145,24 +218,38 @@ int	contentchecker(char c)
 		return (-1);
 }
 
-int	mapchecker(char *mapname)
+int	mapchecker(char *mapname)//remplacer par utilisation gnl
 {
 	int		fd;
 	int		octet;
 	char	buff[2];
+	int		rect[2];
+	char	*map;
 
 	fd = open(mapname, O_RDONLY);
 	if (fd == -1)
 		perror("erreur ouverture map");
 	octet = 1;
+	rect[0] = 0;
+	rect[1] = 0;
 	while (octet == 1)
 	{
+		
 		octet = read(fd, buff, 1);
 		buff[1] = 0;
 		if (charchecker(buff[0]) == -1)
 		{
 			close(fd);
 			return (-1);
+		}
+		if (buff[0] != '\n')
+			rect[0]++;
+		else
+		{
+			if (rect[1] != rect[0] && rect[1] != 0)//si taille ligne i != taille ligne i-1
+				return (-1);
+			rect[1] = rect[0];
+			rect[0] = 0;
 		}
 		contentchecker(buff[0]);
 	}
